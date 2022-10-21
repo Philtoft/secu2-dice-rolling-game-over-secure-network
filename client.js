de8f4ef2c4b822b85375143f32f998b28e7e60ab
Philip August Hyltoft <philip.hyltoft@gmail.com>
2022-10-21 11:12:25 +0200

    fix: gets values from .env file

--- client.js
+++ client.js
@@ -8,8 +8,8 @@ const port = 8888;
 const client1 = new net.Socket();
 
 // generates weird outout...
-let p = "925f15d93a513b441a78826069b4580e3ee37fc5";
-let g = "959144013c88c9782d5edd2d12f54885aa4ba687";
+let p = process.env.P;
+let g = process.env.G;
 let r = "e93c58e6f7f3f4b6f6f0e55f3a4191b87d58b7b1";
 let secret = "1184c47884aeead9816654a63d4209d6e8e906e29";
 
